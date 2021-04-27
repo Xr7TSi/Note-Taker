02c1a4b55ab34f71c1eda097368727250551da84
Jonathan Rein <jrein1296@gmail.com>
2021-04-27 14:37:32 -0400

    Updates to server.js.  File works.

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
+const http = require('http');
 
 
-app.listen(PORT, () => {
+const handleRequest = (request, response) => {
+    response.end(`It Works!! Path Hit: ${request.url}`);
+  };
+
+const server = http.createServer(handleRequest);
+
+server.listen(PORT, () => {
   console.log(`App is currently running on port ${PORT}`)
 });

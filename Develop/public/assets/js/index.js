1850ec5204c399f9cc63a8fb1177d3cc130afaea
Jonathan Rein <jrein1296@gmail.com>
2021-04-27 15:23:25 -0400

    First route / works.

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -6,7 +6,7 @@ let noteList;
 
 
 
-if (window.location.pathname === './notes.html') {
+if (window.location.pathname === './notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -28,7 +28,7 @@ const hide = (elem) => {
 let activeNote = {};
 
 const getNotes = () =>
-  fetch('/api/notes.html', {
+  fetch('/api/notes', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
@@ -36,7 +36,7 @@ const getNotes = () =>
   });
 
 const saveNote = (note) =>
-  fetch('/api/notes.html', {
+  fetch('/api/notes', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -45,7 +45,7 @@ const saveNote = (note) =>
   });
 
 const deleteNote = (id) =>
-  fetch(`/api/notes.html/${id}`, {
+  fetch(`/api/notes/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
@@ -119,7 +119,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes.html') {
+  if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -164,7 +164,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes.html') {
+  if (window.location.pathname === '/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -172,7 +172,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes.html') {
+if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const app = express();
+var path = require('path');
 const PORT = process.env.PORT || 4000;
-const http = require('http');
+const fs = require('fs');
 
 
-const handleRequest = (request, response) => {
-    response.end(`It Works!! Path Hit: ${request.url}`);
-  };
 
-const server = http.createServer(handleRequest);
 
-server.listen(PORT, () => {
-  console.log(`App is currently running on port ${PORT}`)
+// routes
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, '../Note-Taker/Develop/public','notes.html'));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../Note-Taker/Develop/public','index.html'));
+});
+
+
+// C:\Users\jrein\gt-bootcamp\homework\Note-Taker\Develop\public\index.html
+
+app.listen(PORT, () => {
+  console.log(`App is currently running on port ${PORT}`);
 });

0cead83f589148a2d22e88e25ed9d4bfead4cb96
Jonathan Rein <jrein1296@gmail.com>
2021-04-27 21:04:24 -0400

    Updates to api notes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+const database = require("./db/db")
 const PORT = process.env.PORT || 4000;
 const fs = require("fs");
 const { notStrictEqual } = require("assert");
@@ -8,25 +9,21 @@ const notes = {};
 // const notes = "Test";
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
 // routes
 app.get("/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "./public", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
 app.get("/notes", (req, res) => {
-  res.sendFile(
-        
-    path.join(__dirname, "./public", "notes.html")
-  );
+  res.sendFile(path.join(__dirname, "./public", "notes.html"));
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(__dirname, "./db", "db.json")
-  });
+// below route work.  Research why that is.
+app.route("/api/notes").get(function (req, res) {
+  res.json(database);
+});
 
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;

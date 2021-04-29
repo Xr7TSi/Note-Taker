43895e5aac7ed3551a1a91a62eea4333b7901488
Jonathan Rein <jrein1296@gmail.com>
2021-04-28 21:24:55 -0400

    Added fs.writefile db.json.  App appears to function but throws error.

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ var path = require("path");
 const PORT = process.env.PORT || 4000;
 const fs = require("fs");
 const { notStrictEqual } = require("assert");
-const dbFile = require("./db/db.json")
-const uniqId = require('uniqid');
+const dbFile = require("./db/db.json");
+const uniqId = require("uniqid");
 const { stringify } = require("querystring");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public"));
-
 
 // routes
 app.get("/", (req, res) => {
@@ -21,17 +20,21 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "notes.html"));
 });
 
-app.get("/api/notes", (req,res) => {
-  res.json(require('./db/db.json'));
+app.get("/api/notes", (req, res) => {
+  res.json(require("./db/db.json"));
 });
 
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
-  const id = uniqId()
+  const id = uniqId();
   console.log("Crating a new note.");
   newNote.id = id;
   dbFile.push(newNote);
-  console.log(dbFile)
+  console.log(dbFile);
+  // fs.writeFile("./db/db.json", (dbFile.toString()), (err) =>
+  // err ? console.error(err) : console.log("Database updated!"));
+  fs.writeFile("./db/db.json", dbFile, (err) =>
+  err ? console.error(err) : console.log("Database updated!"));
 });
 
 app.listen(PORT, () => {

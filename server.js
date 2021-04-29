3deb56d74d9bf32efda3f7a6dcbf574e6c7a1ff1
Jonathan Rein <jrein1296@gmail.com>
2021-04-29 13:16:03 -0400

    Cleaning up code.

--- server.js
+++ server.js
@@ -3,27 +3,28 @@ const app = express();
 var path = require("path");
 const PORT = process.env.PORT || 4000;
 const fs = require("fs");
-const { notStrictEqual } = require("assert");
 const dbFile = require("./db/db.json");
 const uniqId = require("uniqid");
-const { stringify } = require("querystring");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("./public"));
 
-// routes
+// home route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "index.html"));
 });
 
+//notes page route
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public", "notes.html"));
 });
 
+//route to get database
 app.get("/api/notes", (req, res) => {
   res.json(require("./db/db.json"));
 });
 
+//route for database posting
 app.post("/api/notes", (req, res) => {
   const newNote = req.body;
   const id = uniqId();
@@ -32,17 +33,17 @@ app.post("/api/notes", (req, res) => {
   dbFile.push(newNote);
   console.log(dbFile);
   fs.writeFile("./db/db.json", JSON.stringify(dbFile), (err) =>
+    // res.redirect("/notes") was added to fix bug requiring refresh after note input to display note on left.
     err ? console.error(err) : res.redirect("/notes")
   );
 });
 
-app.delete("/api/notes/:id", function (req, res) {
+app.delete("/api/notes/:id", (req, res) => {
   let jsonFilePath = path.join(__dirname, "/db/db.json");
 
   for (let i = 0; i < dbFile.length; i++) {
     if (dbFile[i].id == req.params.id) {
       dbFile.splice(i, 1);
-      break;
     }
   }
 
@@ -50,12 +51,11 @@ app.delete("/api/notes/:id", function (req, res) {
     if (err) {
       return console.log(err);
     } else {
-      console.log("Your note was deleted!");
+      console.log("Note deleted.");
     }
   });
   res.json(dbFile);
 });
-
 
 app.listen(PORT, () => {
   console.log(`App is currently running on port ${PORT}`);
